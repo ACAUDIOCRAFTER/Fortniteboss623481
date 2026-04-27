be4a0ab1b3de573ba64f48a418d00aca7e642d12
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-04-27 14:12:03 -0700

    Create audiocrafter.js

---
+++ api/audiocrafter.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+    const accept = req.headers.accept || "";
+
+    if (accept.includes("text/html")) {
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+    }
+
+    res.setHeader("Content-Type", "text/plain");
+
+    res.send(`print("hello world")`);
+}
